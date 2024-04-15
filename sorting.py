83cd98323595b3caad45480c90e31f46c13f31a8
Matej Pakán <247286@vutbr.cz>
2024-04-15 10:09:54 +0200

    init2

--- sorting.py
+++ sorting.py
@@ -27,15 +27,55 @@ def read_data(file_name):
     
     return data
 
-def bubble_sort(data):
-    sort_field = 
+def selection_sort(sel_array,direction="vzostupne"):
+    """
+    """
+    for i in range(len(sel_array)-1):
+        if direction == "vzostupne":
+            min_idx = i
+            for j in range(i+1, len(sel_array)):
+                if sel_array[j] < sel_array[min_idx]:
+                    min_idx = j
 
+            sel_array[i], sel_array[min_idx] = sel_array[min_idx], sel_array[i]
+        
+        elif direction == "zostupne":
+            max_idx = i
+            for j in range(i+1, len(sel_array)):
+                if sel_array[j] > sel_array[max_idx]:
+                    max_idx = j
+            
+            sel_array[i], sel_array[max_idx] = sel_array[max_idx], sel_array[i]
+    return sel_array
+
+def bubble_sort(bubble_arr):
+    """
+    """
+    swapped = True
+    while swapped:
+        swapped = False
+
+        for i in range(len(bubble_arr)-1):
+            if bubble_arr[i] > bubble_arr[i+1]:
+                bubble_arr[i], bubble_arr[i+1] = bubble_arr[i+1], bubble_arr[i]
+                swapped = True
+
+    return bubble_arr
+
+def insertion_sort():
+    ...
+
+    
 
 def main():
     #pass
     file_name = 'numbers.csv'
     data = read_data(file_name)
     print(data)
+
+    bubble_sauce = data['series_1']
+    bubble = bubble_sort(bubble_sauce)
+    print(bubble)
 
 if __name__ == '__main__':
     main()
